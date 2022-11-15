198bb09cf586c81946d7d497f90882d55fddbdab
Leila Rivarola <leydnr_lara@hotmail.com>
2022-11-15 00:04:04 -0300

    cambios

--- index.js
+++ index.js
@@ -10,26 +10,6 @@ function saludoGenerico() {
 alert("Bienvenido")
 saludoGenerico()
 
-class Producto {
-    constructor(id, nombre, precio,stock) {
-        this.id = id;
-        this.nombre = nombre
-        this.precio = precio
-        this.stock = stock
-    }
-    restaStock(){
-        this.stock = this.stock - 1;
-        console.log(`El Stock de {this.nombre} ha sido actualizado`)
-    }
-}
-
-const lasTortugas  = new Producto(0, 'Las Tortugas', 1000, 15);
-const donNicanor = new Producto(1, 'Don Nicanor', 1200,30);
-const patrimonial  = new Producto(2, 'Patrimonial', 1500,40);
-const nietoSenetiner  = new Producto(3, 'Nieto Senetiner', 2000,10);
-
-const productos = [lasTortugas, donNicanor,patrimonial,nietoSenetiner];
-
 const carrito = []
 let producto = parseInt(prompt('Elige el que deseas comprar: 0.Las Tortugas - 1.Don Nicanor - 2.Patrimonial - 3.Nieto Senetiner'))
 
@@ -38,6 +18,27 @@ let seguirEligiendo = true
 let decision
 
 const stock = []
+
+class Producto {
+    constructor(id, nombre, precio, stock) {
+        this.id = id;
+        this.nombre = nombre
+        this.precio = precio
+        this.stock = stock
+    }
+}
+
+const lasTortugas = new Producto(0, 'Las Tortugas', 1000, 15);
+stock.push(lasTortugas)
+const donNicanor = new Producto(1, 'Don Nicanor', 1200, 30);
+stock.push(donNicanor)
+const patrimonial = new Producto(2, 'Patrimonial', 1500, 40);
+stock.push(patrimonial)
+const nietoSenetiner = new Producto(3, 'Nieto Senetiner', 2000, 10);
+stock.push(nietoSenetiner)
+
+const productos = [lasTortugas, donNicanor, patrimonial, nietoSenetiner];
+
 
 while (seguirEligiendo === true) {
     if (producto === 1) {
@@ -65,6 +66,20 @@ while (seguirEligiendo === true) {
 
 alert(`El total se tu compra es ${totalCompra}`)
 
+function creaProducto() {
+    let prodNombre = prompt("Nombre del producto a crear")
+    let prodPrecio = parseInt(prompt("Valor del producto a crear"))
+    let prodStock = parseInt(prompt("Cantidad del producto a crear"))
+    while (prodNombre == "" || isNaN(prodPrecio) || isNaN(prodStock)) {
+        alert("Alguno de los datos no es válido, reintente.")
+        prodNombre = prompt("Nombre del producto a crear")
+        prodPrecio = parseInt(prompt("Valor del producto a crear"))
+        prodStock = parseInt(prompt("Cantidad del producto a crear"))
+    }
+    const prodManual = new Producto (4, prodNombre, prodPrecio, prodStock)
 
+    productos.push(prodManual)
+    agregarCarrito()
+}
 
-stock.push(nietoSenetiner)
+creaProducto()
